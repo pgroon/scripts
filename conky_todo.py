92f158c7a66eb26211de2e945375bc7728e1aad0
groon <schaubph@gmail.com>
2025-07-03 21:41:42 +0200

    added example files for conky_todo.py script

--- conky_todo.py
+++ conky_todo.py
@@ -1,30 +1,18 @@
 import re
 
-def generate_conkyrc(input_file, output_file):
-    # Read lines from the input text file
+def generate_conkyrc(input_file):
+    # Read paragraphs from the input text file
     with open(input_file, 'r') as file:
-        lines = file.read().split('\n')  # Currently does not support multiline-todos
-		
-    # Format lines as bullet points
-    bullet_points = ['${voffset 10}$alignr • ' + re.sub(r'\n', '$alignr\n       ', line) for line in lines]
+        paragraphs = file.read().split('\n\n')  # Assuming paragraphs are separated by two newline characters
 
-    # Read the existing content of the .conkyrc file
-    with open(output_file, 'r') as file:
-        conkyrc_content = file.read()
+    # Format paragraphs as bullet points
+    # "template0" is defined within .conkyrc
+    bullet_points = ['${template0}  ' + re.sub(r'\n', '\n       ', paragraph) for paragraph in paragraphs]
 
-    # Find the positions of #todo-start and #todo-end
-    start_pos = conkyrc_content.find('#todo_start') + len('#todo_start')
-    end_pos = conkyrc_content.find('#todo_end')
-
-    # Replace the content between #todo-start and #todo-end with the formatted bullet points
-    updated_conkyrc = conkyrc_content[:start_pos] + '\n'.join(bullet_points) + conkyrc_content[end_pos:]
-
-    # Write the updated content to the .conkyrc file
-    with open(output_file, 'w') as file:
-        file.write(updated_conkyrc)
+    # Print the formatted bullet points to stdout
+    print('\n'.join(bullet_points))
 
 if __name__ == "__main__":
-    input_file_path = '/home/pgroon/Downloads/todo.md'   # Change this to the path of your input text file
-    output_file_path = '/home/pgroon/.conkyrc'    # Change this to the desired output .conkyrc file name
+    input_file_path = '/home/groon/Downloads/todo.md'   # Change this to the path of your input text file
 
-    generate_conkyrc(input_file_path, output_file_path)
+    generate_conkyrc(input_file_path)
---
+++ example_todo.md
@@ -0,0 +1,9 @@
+Just add your todos
+
+like this, with a free line in between.
+
+Your .conkyrc will call conky_todo.py
+
+conky_todo.py will read this
+
+and display the todos on your desktop.
